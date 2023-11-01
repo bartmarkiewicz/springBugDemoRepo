4a8bdc323b33a300654da71386f7b9dca41cd6b5
bmarkiewicz <bartmark322@gmail.com>
2023-11-01 13:59:52 +0000

    Initial commit

---
+++ src/main/java/com/example/springbugdemorepo/SpringBugDemoRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbugdemorepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBugDemoRepoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBugDemoRepoApplication.class, args);
+  }
+
+}
